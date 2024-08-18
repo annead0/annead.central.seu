afd3ef2622f588aa927131e3070c3b16ae8169ee
annead0 <mikael12zamorano@outlook.com>
2024-08-18 18:46:02 +0000

    update

---
+++ annead.seu/pages/index.vue
@@ -0,0 +1,23 @@
+<script>
+import { USelectMenu } from '#build/components';
+var langU = false;
+const lang =[{id:1 ,name:"EN"},{id:2,name:"CAT/VAL"}];
+const uiLang = ref(lang[0].id);
+if(uiLang.id === 1){
+    langU = true;
+}else{
+    langU = false;
+};
+</script>
+<template>
+    <USelectMenu v-model="uiLang" :options="lang"></USelectMenu>
+    <UCard class="mt-20" align="left" style="align-items:left;">
+      <article>
+        <div v-if="langU">
+            Our new digital headquarters
+        </div>
+        <div v-else>
+        La Nostra nova seu</div>
+      </article>
+    </UCard>
+</template>
