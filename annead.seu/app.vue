adb98e16c2dfcb6f806d615a0bc37c1d1bfec546
annead0 <mikael12zamorano@outlook.com>
2024-08-18 18:03:51 +0000

    update

--- annead.seu/app.vue
+++ annead.seu/app.vue
@@ -1,9 +1,20 @@
 <template>
-  <UContainer>
+  <UContainer class=".moderustic">
     <UCard class="mt-10">
-      <template #header>
+      <template #header >
         Annead.
       </template>
     </UCard>
   </UContainer>
 </template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Moderustic:wght@300..800&display=swap');
+
+
+.moderustic {
+  font-family: "Moderustic", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 600;
+  font-style: normal;
+}
+</style>
