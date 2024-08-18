0f937cbd045fc84321710f6e16ea12bf9e22d24b
annead0 <mikael12zamorano@outlook.com>
2024-08-18 19:37:01 +0000

    update

--- annead.seu/pages/home.vue
+++ annead.seu/pages/home.vue
@@ -1,14 +1,12 @@
 <script lang="ts">
-import { USelectMenu } from '#build/components';
+
 
 </script>
 <template>
-    <USelectMenu v-model="uiLang" :options="lang"></USelectMenu>
     <UCard class="mt-20" align="left" style="align-items:left;">
       <article>
-       
-        <div >
-        Our new digital headquarters.</div>
+    
+        Our new digital headquarters.
       </article>
     </UCard>
 </template>
